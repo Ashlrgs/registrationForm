ebd3c99e8c18c006eb92990eb07f483161b47ef4
Ashlesha Bhandarkar <117961272+Ashlrgs@users.noreply.github.com>
2024-05-13 23:57:01 +0530

    Add files via upload

---
+++ registerjs.js
@@ -0,0 +1,9 @@
+function showCard() { 
+    document.getElementById("card_section").style.display =  "block";
+    document.getElementById("upi_section").style.display =  "none"; 
+   } 
+   function showUPI() { 
+    document.getElementById("card_section").style.display =  "none"; 
+    document.getElementById("upi_section").style.display =  "block"; 
+   } 
+   
